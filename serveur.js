1f2d9bb3d6ee04b8a1ff4f4f6bd3d0458218afe9
Mohamed Konate <ing.mohamedkonate@gmail.com>
2020-09-06 23:16:20 +0000

    Les routes - Routes dynamiques

--- serveur.js
+++ serveur.js
@@ -16,6 +16,11 @@ app.get('/etage/1/chambre', function(req, res) {
     res.send('Hé ho, c\'est privé ici !');
 });
 
+app.get('/etage/:etagenum/chambre', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes à la chambre de l\'étage n°' + req.params.etagenum);
+});
+
 app.use(function(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
     res.status(404).send('Page introuvable !');
