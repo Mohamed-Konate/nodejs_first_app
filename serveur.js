e18f10cfd6b4f3799c6c2857c20ea870552cb7b4
Mohamed Konate <ing.mohamedkonate@gmail.com>
2020-09-06 23:48:40 +0000

    Les templates - #Plusieurs paramètres et des boucles

--- serveur.js
+++ serveur.js
@@ -20,6 +20,11 @@ app.get('/etage/:etagenum/chambre', function(req, res) {
     res.render('chambre.ejs', {etage: req.params.etagenum});
 });
 
+app.get('/compter/:nombre', function(req, res) {
+    var noms = ['Robert', 'Jacques', 'David'];
+    res.render('page.ejs', {compteur: req.params.nombre, noms: noms});
+});
+
 app.use(function(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
     res.status(404).send('Page introuvable !');
